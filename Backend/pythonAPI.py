56179277dacd87bc1064ece82d2d3c79054a7f91
Alex Ng <ang98320@gmail.com>
2017-09-16 17:30:01 -0400

    Changed placeholder values to live values

--- Backend/pythonAPI.py
+++ Backend/pythonAPI.py
@@ -22,29 +22,29 @@ def cuisineTranslate(cuisine):
 #Pass lat long every 30 seconds to array, remove oldest if does average not fufill deviation of .004 degree 
 #pastLocations 
 
-"""
-lat = str(geolocator.latitude)
-lon = str(geolocator.longitude)
-cuisineType = cuisineTranslate(application.cuisineField) #check against array and convert to numerical value
-distance = str(application.distanceField * 1000) #distance will be in km, Zomato api needs in meters
+
+searchLat = str(lat)
+searchLon = str(lon)
+cuisineType = cuisineTranslate(cuisine) #check against array and convert to numerical value
+searchdistance = str(distance * 1609) #distance will be in km, Zomato api needs in meters
 """
 lat = str(43.472285)
 lon = str(-80.544858)
 cuisineType = str(55)
 distance = str(1 * 1000)
-
+"""
 #adds lat and long based on profiles current location
-mapUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lon + "&key=AIzaSyCN1nbMMdIdWb9o2sfr4MVV1AZVlJ8G_Ug"
+mapUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + searchlat + "," + searchlon + "&key=AIzaSyCN1nbMMdIdWb9o2sfr4MVV1AZVlJ8G_Ug"
 
 #uses urllib2 library to open and load json file
 mapResponse = urllib2.urlopen(mapUrl)
 mapData = json.load(mapResponse)
 
 #creates query needed for Zomato 
-restQuery = "lat=" + lat + ", lon=" + lon + ", radius =" + distance + ", cuisines=" + cuisineType
+restQuery = "lat=" + searchLat + ", lon=" + searchLon + ", radius =" + searchDistance + ", cuisines=" + cuisineType
 restKey = Zomato("309bf0bce94239a8585b1b209da93a3d")
 restJSON = restKey.parse("search", restQuery)
 
-print "Break"
+
 print restJSON
 
