4c5bb8fb95bf5babee5ff932c98758c28f9f64d0
Nilay Shah <nilay.shah19@gmail.com>
2017-09-17 06:10:20 -0400

    Saved variables to be passed to server.

--- AndroidFrontEnd/FoodRecommendation/app/src/main/java/com/example/nilay/foodrecommendation/Options.java
+++ AndroidFrontEnd/FoodRecommendation/app/src/main/java/com/example/nilay/foodrecommendation/Options.java
@@ -60,6 +60,34 @@ public class Options extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                int price;
+                int distance;
+                int time;
+                String cuisine;
+
+                // int for price
+                Spinner p = (Spinner) findViewById(R.id.priceSpinner);
+                price = p.getSelectedItem().toString().length();
+                Spinner d = (Spinner) findViewById(R.id.distanceSpinner);
+                // int for distance
+                String sDistance = d.getSelectedItem().toString();
+                if(sDistance.length() == 3)
+                    distance = 24;
+                else
+                    distance = Integer.parseInt(sDistance);
+                // int for time
+                Spinner t = (Spinner) findViewById(R.id.timeSpinner);
+                String sTime = t.getSelectedItem().toString();
+                if(sTime.equals("1 hr from now"))
+                    time = 1;
+                else if(sTime.equals("2 hrs from now"))
+                    time = 2;
+                else if(sTime.equals("3 hrs from now"))
+                    time = 3;
+                // string for cuisine
+                Spinner f = (Spinner) findViewById(R.id.feelsSpinner);
+                cuisine = f.getSelectedItem().toString();
+
                 startActivity(new Intent(Options.this, Dashboard.class));
             }
         });
