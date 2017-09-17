186290e4294041a46d28dcfd78635f407d4f127b
Terrence Ho <terrenceho.books@gmail.com>
2017-09-17 00:34:33 -0400

    -Android commits

--- Backend/app.py
+++ Backend/app.py
@@ -16,7 +16,6 @@ db = SQLAlchemy(app)
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    # email = db.Column(db.String(120), unique=True)
     firstname = db.Column(db.String(120))
     lastname = db.Column(db.String(120))
     tf_path = db.Column(db.String(500))
